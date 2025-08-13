d2146f81a74fa5626d699322f3551cb481945ec3
Emin Bahous <emin.bahous@le-campus-numerique.fr>
2025-08-13 09:21:23 +0200

    update

--- src/Demenagement.java
+++ src/Demenagement.java
@@ -1,15 +1,41 @@
+import java.util.Scanner;
+/**
+ * The main class of the program in Java
+ */
 public class Demenagement {
     public static void main (String[] args) {
-        int totalBox = 34;
-        int truckCapacity = 9;
-        int remainBox = totalBox;
-        int travel = 1;
+        Scanner scan = new Scanner(System.in);
 
-        while (remainBox > 0) {
-            int boxInTravel = Math.min(remainBox, truckCapacity);
-            System.out.println("voyage de " +  boxInTravel + " cartons");
-            remainBox -= boxInTravel;
-            travel++;
+        int totalBox = 0;
+        int truckCapacity =0;
+        int inputWait = 1;
+
+        while ( inputWait <= 2) {
+            System.out.print(inputWait == 1 ? "Entrez le nombre de boites: " : "Entrez le nombre de capacité: ");
+
+        if (scan.hasNextInt()){
+            int input = scan.nextInt();
+
+            if (input > 0) {
+                if (inputWait == 1){
+                    totalBox = input;
+                }else {
+                    truckCapacity = input;
+                }
+                inputWait++;
+            }
+            }
         }
+        int remainingBox = totalBox;
+        int travelNumber = 1;
+
+        while (remainingBox > 0){
+        int boxThisTravel = Math.min(remainingBox, truckCapacity);
+        System.out.println("Voyage " + travelNumber + ": " + boxThisTravel);
+        remainingBox -= boxThisTravel;
+        travelNumber++;
+        }
+        scan.close();
+
     }
-}
+    }
